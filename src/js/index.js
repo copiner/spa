fd094bfa01e7b9a31867621277a9db68ea315297
copiner <wdaonngg@gmail.com>
2019-10-23 12:06:39 +0800

    gog

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,31 @@
-window.onload = function(){
+var indexPage = new Page("index");
+console.log(indexPage)
+indexPage.getDomObj = function(){
+  this.btnGroup = this.domElement.querySelector(".btn-group");
+  //console.log(this.domElement.querySelector(".btn-group"))
+  this.btnGroup.addEventListener("click",this.clickHandler, false);
+};
 
-  var indexPage = new Page("index");
-  //console.log(indexPage)
-  indexPage.getDomObj = function(){
-    this.btnGroup = this.domElement.querySelector(".btn-group");
+indexPage.removeDomObj = function(){
+  this.btnGroup.removeEventListener("click",this.clickHandler,false);
+  this.btnGroup = null;
+};
 
-    //this.btnGroup.addEventListener("click",this.clickHandler, false);
-  };
+indexPage.clickHandler = function(ev){
+  var target = ev.target;
+  var action = target.dataset.action;
+  switch (action) {
+    case "register":
+      indexPage.unit();
+      registerPage.init(pageContainer);
+      break;
+    case "login":
+      indexPage.unit();
+      loginPage.init(pageContainer);
+      break;
+    default:
 
-  indexPage.removeDomObj = function(){
-    this.btnGroup.removeEventListener("click",this.clickHandler,false);
-    this.btnGroup = null;
-  };
-
-  indexPage.clickHandler = function(ev){
-    var target = ev.target;
-    var action = target.dataset.action;
-    switch (action) {
-      case "register":
-        indexPage.unit();
-        registerPage.init(pageContainer);
-        break;
-      case "login":
-        indexPage.unit();
-        loginPage.init(pageContainer);
-        break;
-      default:
-
-    }
   }
-  var pageContainer = document.querySelector(".pageContainer");
-  indexPage.init(pageContainer);
 }
+var pageContainer = document.querySelector(".pageContainer");
+indexPage.init(pageContainer);
